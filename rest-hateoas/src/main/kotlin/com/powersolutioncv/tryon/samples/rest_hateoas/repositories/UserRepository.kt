ea0d12f70fd1e723c8207c455a7b257885ae4bbe
anilson <anilsonmonteiro@gmail.com>
2021-05-29 15:28:20 -0100

    persistence on db implementation and starting doockerfile

--- rest-hateoas/src/main/kotlin/com/powersolutioncv/tryon/samples/rest_hateoas/repositories/UserRepository.kt
+++ rest-hateoas/src/main/kotlin/com/powersolutioncv/tryon/samples/rest_hateoas/repositories/UserRepository.kt
@@ -1,7 +1,6 @@
 package com.powersolutioncv.tryon.samples.rest_hateoas.repositories
 
 import com.powersolutioncv.tryon.samples.rest_hateoas.models.User
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
 interface UserRepository: CrudRepository<User, Long>
